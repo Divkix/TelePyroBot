41e0a1d7ffe25e329907575044af104217e1025f
Skuzzy xD <techdroidroot@gmail.com>
2020-06-05 13:23:54 +0530

    added start to string when restated

--- pyrobot/plugins/tools/powertools.py
+++ pyrobot/plugins/tools/powertools.py
@@ -7,5 +7,5 @@ from pyrobot.helper_functions.cust_p_filters import owner_filter
 async def updater(client, message):
     await message.reply_text(
         "Restarted! "
-        f"Do `{COMMAND_HAND_LER}alive` to check if I am online...")
+        f"Do `{COMMAND_HAND_LER}alive` or `{COMMAND_HAND_LER}start` to check if I am online...")
     await client.restart()

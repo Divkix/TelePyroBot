a428b4476c5e6b9e79689ecca2bedbcfb2d0b155
Skuzzy xD <techdroidroot@gmail.com>
2020-06-05 18:27:17 +0530

    added reboot

--- pyrobot/plugins/tools/powertools.py
+++ pyrobot/plugins/tools/powertools.py
@@ -1,5 +1,7 @@
+import time
 import asyncio
-from pyrobot import COMMAND_HAND_LER
+import shutil
+from pyrobot import COMMAND_HAND_LER, HEROKU_APP
 from pyrogram import Client, Filters
 from pyrobot.helper_functions.cust_p_filters import sudo_filter
 
@@ -9,3 +11,13 @@ async def updater(client, message):
         "Restarted! "
         f"Do `{COMMAND_HAND_LER}alive` or `{COMMAND_HAND_LER}start` to check if I am online...", parse_mode="md")
     await client.restart()
+
+
+@Client.on_message(Filters.command("reboot", COMMAND_HAND_LER) & sudo_filter)
+async def restart_cmd_handler(client, message):
+    rm = await message.edit("Restarting Userbot Services")
+    asyncio.wait(1)
+    await rm.edit("Finalizing...")
+    asyncio.wait(3)
+    rm.delete()
+    asyncio.get_event_loop().create_task(client.restart())

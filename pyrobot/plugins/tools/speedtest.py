5ad671ba15afa0b942e5ce35edc8843b84ccafb1
Skuzzy xD <techdroidroot@gmail.com>
2020-06-05 14:53:32 +0530

    Added sme things

---
+++ pyrobot/plugins/tools/speedtest.py
@@ -0,0 +1,40 @@
+from pyrogram import Client, Filters
+
+from pyrobot import COMMAND_HAND_LER
+from pyrobot.helper_functions.cust_p_filters import sudo_filter
+from speedtest import Speedtest
+
+@Client.on_message(Filters.command("speedtest", COMMAND_HAND_LER) & sudo_filter)
+async def listbots(client, message):
+    await message.edit("`Running speed test . . .`")
+    test = Speedtest()
+    test.get_best_server()
+    test.download()
+    test.upload()
+    test.results.share()
+    result = test.results.dict()
+    await message.edit("`"
+                       "Started at "
+                       f"{result['timestamp']} \n\n"
+                       "Download "
+                       f"{speed_convert(result['download'])} \n"
+                       "Upload "
+                       f"{speed_convert(result['upload'])} \n"
+                       "Ping "
+                       f"{result['ping']} \n"
+                       "ISP "
+                       f"{result['client']['isp']}"
+                       "`")
+
+
+def speed_convert(size):
+    """
+    Hi human, you can't read bytes?
+    """
+    power = 2 ** 10
+    zero = 0
+    units = {0: '', 1: 'Kb/s', 2: 'Mb/s', 3: 'Gb/s', 4: 'Tb/s'}
+    while size > power:
+        size /= power
+        zero += 1
+    return f"{round(size, 2)} {units[zero]}"

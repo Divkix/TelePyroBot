fc6bcc80258737f0daa97a806c3bb9d0b59cb9cf
Skuzzy xD <techdroidroot@gmail.com>
2020-06-06 16:40:31 +0530

    import app

--- pyrobot/__main__.py
+++ pyrobot/__main__.py
@@ -4,7 +4,10 @@ if __name__ == "__main__":
     app = PyroBot()
     app.run()"""
 
+
+#Remove afterwards.............
 import asyncio
+from pyrobot import app
 loop = asyncio.get_event_loop()
 
 async def start_bot():
@@ -12,3 +15,4 @@ async def start_bot():
 
 if __name__ == '__main__':
     loop.run_until_complete(start_bot())
+#Remove afterwards.............

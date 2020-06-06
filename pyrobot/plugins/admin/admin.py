4f87fe45a30355363c9d0017273a6120377f412c
Skuzzy xD <techdroidroot@gmail.com>
2020-06-06 10:24:06 +0530

    Added chat plugin + demote feature

---
+++ pyrobot/plugins/admin/admin.py
@@ -0,0 +1,70 @@
+import asyncio
+from pyrogram import Client, Filters
+from pyrobot import COMMAND_HAND_LER
+from pyrobot.helper_functions.cust_p_filters import sudo_filter
+from pyrobot.helper_functions.admin_check import admin_check
+
+@Client.on_message(Filters.command("promote", COMMAND_HAND_LER) & sudo_filter)
+async def promote_usr(client, message):
+    rm = await message.reply_text("`Trying to Promote User.. Hang on!! ⏳`", parse_mode="md")
+    is_admin = await admin_check(message)
+
+    if not is_admin:
+        return
+
+    if message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+    else:
+        args = message.input_str.split(maxsplit=1)
+        if len(args) == 1:
+            user_id = args[0]
+        else:
+            await rm.edit("`no valid user_id or message specified,`", parse_mode="md")
+
+    if user_id and chat_id:
+        try:
+            await client.promote_chat_member(chat_id, user_id,
+                                            can_change_info=False,
+                                            can_delete_messages=True,
+                                            can_restrict_members=True,
+                                            can_invite_users=True,
+                                            can_pin_messages=True)
+            await asyncio.sleep(2)
+            await rm.edit("`👑 Promoted Successfully..`", parse_mode="md")
+        except Exception as ef:
+            await rm.edit(
+                text="`something went wrong`\n\n"
+                f"**ERROR:** `{ef}`", parse_mode="md")
+
+
+@Client.on_message(Filters.command("demote", COMMAND_HAND_LER) & sudo_filter)
+async def demote_usr(client, message):
+    rm = await message.reply_text("`Trying to Demote User.. Hang on!! ⏳`", parse_mode="md")
+    is_admin = await admin_check(message)
+
+    if not is_admin:
+        return
+
+    if message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+    else:
+        args = message.input_str.split(maxsplit=1)
+        if len(args) == 1:
+            user_id = args[0]
+        else:
+            await rm.edit(`no valid user_id or message specified,`", parse_mode="md")
+
+    if user_id and chat_id:
+        try:
+            await client.promote_chat_member(chat_id, user_id,
+                                            can_change_info=False,
+                                            can_delete_messages=False,
+                                            can_restrict_members=False,
+                                            can_invite_users=False,
+                                            can_pin_messages=False)
+            await asyncio.sleep(2)
+            await rm.edit("`Demoted Successfully..`", parse_mode="md")
+        except Exception as ef:
+            await rm.edit(
+                text="`something went wrong`\n\n"
+                f"**ERROR:** `{ef}`", parse_mode="md")
---
+++ pyrobot/plugins/admin/chat.py
@@ -0,0 +1,52 @@
+from pyrogram import Client, Filters
+from pyrobot import COMMAND_HAND_LER
+from pyrobot.helper_functions.cust_p_filters import sudo_filter
+from pyrobot.helper_functions.admin_check import admin_check
+
+
+@Client.on_message(Filters.command("leavechat", COMMAND_HAND_LER) & sudo_filter)
+async def leavechat(client, message):
+    if message.chat.type in ['group', 'supergroup']:
+        chat_id = message.chat.id
+        is_admin = await admin_check(message)
+        if not is_admin:
+            return
+        await client.leave_chat(chat_id, delete=True)
+
+
+@Client.on_message(Filters.command("invitelink", COMMAND_HAND_LER) & sudo_filter)
+async def invitelink(client, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+    chat_id = messsage.chat.id
+    link = client.export_chat_invite_link(chat_id)
+    await message.reply_text(f"**Link for Chat:**\n{link}", parse_mode="md")
+
+
+@Client.on_message(Filters.command("delchatpic", COMMAND_HAND_LER) & sudo_filter)
+async def delchatpic(client, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+    chat_id = messsage.chat.id
+    try:
+        await client.delete_chat_photo(chat_id)
+        await message.reply_text("Deleted Chat Pic!")
+    except Exception as ef:
+        await message.reply_text(f"Error deleting Chat Pic due to:\n`{ef}`", parse_mode="md")
+
+
+@Client.on_message(Filters.command("setchatname", COMMAND_HAND_LER) & sudo_filter)
+async def setchatname(client, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+    chat_id = messsage.chat.id
+    args = message.input_str.split(maxsplit=1)
+    if len(args) >= 2:
+        chat_title = args[0:]
+    try:
+        await client.set_chat_title(chat_id, chat_title)
+    except Exception as ef:
+        await client.reply_text(f"**Could not Change Chat Title due to:**\n`{ef}`")

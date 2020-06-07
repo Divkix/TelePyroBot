817021c6d916a686bb871944777c3b7e1bfec487
Skuzzy xD <techdroidroot@gmail.com>
2020-06-07 14:25:34 +0530

    Move modules out of folder

--- pyrobot/plugins/__init__.py
+++ pyrobot/plugins/__init__.py
@@ -4,7 +4,7 @@ def __list_all_modules():
     from os.path import dirname, basename, isfile
     import glob
     # This generates a list of modules in this folder for the * in __main__ to work.
-    mod_paths = glob.glob(dirname(__file__) + "/*/*.py")
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
     all_modules = [basename(f)[:-3] for f in mod_paths if isfile(f)
                    and f.endswith(".py")
                    and not f.endswith('__init__.py')]

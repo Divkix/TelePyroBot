a4acbfeeec58a5e2d71dc2d6e7e93e9274efce64
SkuzzyxD <techdroidroot@gmail.com>
2020-06-11 15:28:42 +0530

    use bold style

--- pyrobot/plugins/dev.py
+++ pyrobot/plugins/dev.py
@@ -130,7 +130,7 @@ async def execution(_, message):
 @Client.on_message(Filters.command("ip", COMMAND_HAND_LER) & Filters.me)
 async def public_ip(client, message):
     ip = requests.get('https://api.ipify.org').text
-    await message.edit(f'Bot IP Address:\n<code>{ip}</code>', parse_mode='html')
+    await message.edit(f'<b>Bot IP Address:</b>\n\n<code>{ip}</code>', parse_mode='html')
 
 
 @Client.on_message(Filters.command("dc", COMMAND_HAND_LER) & Filters.me)

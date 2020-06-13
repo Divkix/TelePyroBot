2737d3c43178d2a20db6b22cc83066e731b9683f
SkuzzyxD <techdroidroot@gmail.com>
2020-06-13 22:10:46 +0530

    uff

--- pyrobot/plugins/aria.py
+++ pyrobot/plugins/aria.py
@@ -24,6 +24,7 @@ aria2 = None
 async def aria_start(client, message):
     cmd = "aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800  --max-connection-per-server=10 --rpc-max-request-size=1024M --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 --daemon=true --allow-overwrite=true --dir='/app/pyrobot/downloads'"
     aria2_is_running = os.system(cmd)
+    global aria2
     aria2 = aria2p.API(aria2p.Client(host="http://localhost", port=6800, secret=""))
     await message.edit(f"**Started Aria Client**")
 

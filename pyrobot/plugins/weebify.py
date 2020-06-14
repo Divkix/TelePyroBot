ab51552c02eae9cb512a452f8b41a7afc37842cc
SkuzzyxD <techdroidroot@gmail.com>
2020-06-14 08:41:18 +0530

    Create weebify.py

---
+++ pyrobot/plugins/weebify.py
@@ -0,0 +1,24 @@
+from pyrogram import Client, Filters
+from pyrobot import COMMAND_HAND_LER
+
+
+normiefont = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+              'v', 'w', 'x', 'y', 'z']
+weebyfont = ['卂', '乃', '匚', '刀', '乇', '下', '厶', '卄', '工', '丁', '长', '乚', '从', '𠘨', '口', '尸', '㔿', '尺', '丂', '丅', '凵',
+             'リ', '山', '乂', '丫', '乙']
+
+
+@Client.on_message(Filters.command("weebify", COMMAND_HAND_LER) & Filters.me)
+async def weebify(client, message):
+    args = message.text.split(" ", 1)[1]
+    if message.reply_to_message:
+        args = message.reply_to_message.text
+    if not args:
+        await event.edit("`What I am Supposed to Weebify U Dumb`")
+        return
+    string = '  '.join(args).lower()
+    for normiecharacter in string:
+        if normiecharacter in normiefont:
+            weebycharacter = weebyfont[normiefont.index(normiecharacter)]
+            string = string.replace(normiecharacter, weebycharacter)
+    await message.edit(string)

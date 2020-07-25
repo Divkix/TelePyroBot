90688e1a577517f057887c344087084a074c1859
SkuzzyxD <techdroidroot@gmail.com>
2020-07-25 22:00:51 +0530

    improve format

--- pyrobot/utils/display_progress_dl_up.py
+++ pyrobot/utils/display_progress_dl_up.py
@@ -22,12 +22,12 @@ async def progress_for_pyrogram(
         elapsed_time = time_formatter(milliseconds=elapsed_time)
         estimated_total_time = time_formatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \nP: {2}%\n".format(
+        progress = "**[{0}{1}]** \n**Progress**: __{2}%__\n".format(
             ''.join(["●" for i in range(math.floor(percentage / 5))]),
             ''.join(["○" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
+        tmp = progress + "**Done:** __{0} of {1}__\n**Speed:** __{2}/s__\n**ETA:** __{3}__\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),

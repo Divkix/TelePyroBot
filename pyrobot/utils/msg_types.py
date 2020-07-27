357ed6ad6ba51461c6b3fa9ad878d1044be6a2eb
Skuzzy xD <techdroidroot@gmail.com>
2020-07-27 16:35:55 +0530

    fix afk

--- pyrobot/utils/msg_types.py
+++ pyrobot/utils/msg_types.py
@@ -7,15 +7,17 @@ from pyrobot.utils.string_handling import button_markdown_parser
 
 @unique
 class Types(IntEnum):
-    TEXT = 0
-    BUTTON_TEXT = 1
-    STICKER = 2
-    DOCUMENT = 3
-    PHOTO = 4
-    AUDIO = 5
-    VOICE = 6
-    VIDEO = 7
+    TEXT = 1
+    DOCUMENT = 2
+    PHOTO = 3
+    VIDEO = 4
+    STICKER = 5
+    AUDIO = 6
+    VOICE = 7
     VIDEO_NOTE = 8
+    ANIMATION = 9
+    ANIMATED_STICKER = 10
+    CONTACT = 11
 
 
 def get_note_type(msg: Message, split: int):
@@ -119,3 +121,42 @@ def get_file_id(msg: Message):
             content = msg.video_note.file_id
             data_type = Types.VIDEO_NOTE
     return data_type, content
+
+def get_message_type(msg):
+    if msg.text or msg.caption:
+        content = None
+        message_type = Types.TEXT
+    elif msg.sticker:
+        content = msg.sticker.file_id
+        message_type = Types.STICKER
+
+    elif msg.document:
+        if msg.document.mime_type == "application/x-bad-tgsticker":
+            message_type = Types.ANIMATED_STICKER
+        else:
+            message_type = Types.DOCUMENT
+        content = msg.document.file_id
+
+    elif msg.photo:
+        content = msg.photo.file_id  # last elem = best quality
+        message_type = Types.PHOTO
+
+    elif msg.audio:
+        content = msg.audio.file_id
+        message_type = Types.AUDIO
+
+    elif msg.voice:
+        content = msg.voice.file_id
+        message_type = Types.VOICE
+
+    elif msg.video:
+        content = msg.video.file_id
+        message_type = Types.VIDEO
+
+    elif msg.video_note:
+        content = msg.video_note.file_id
+        message_type = Types.VIDEO_NOTE
+
+    elif msg.animation:
+        content = msg.animation.file_id
+        message_type = Types.ANIMATION

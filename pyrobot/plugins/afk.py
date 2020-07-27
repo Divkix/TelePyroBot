0e74a551fbb2151806df755b7f662dd5c7443cd0
SkuzzyxD <techdroidroot@gmail.com>
2020-07-27 20:17:53 +0530

    now do lel

--- pyrobot/plugins/afk.py
+++ pyrobot/plugins/afk.py
@@ -86,8 +86,8 @@ async def afk_mentioned(client, message):
     await message.stop_propagation()
 
 
-#@Client.on_message(Filters.me & Filters.group & ~Filters.chat(PRIVATE_GROUP_ID), group=12)
-@Client.on_message((Filters.command("unafk", COMMAND_HAND_LER)) & Filters.me)
+@Client.on_message(Filters.me & Filters.group & ~Filters.chat(PRIVATE_GROUP_ID), group=12)
+#@Client.on_message((Filters.command("unafk", COMMAND_HAND_LER)) & Filters.me)
 async def no_longer_afk(client, message):
     global MENTIONED
     get = get_afk()

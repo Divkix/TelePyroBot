1ea075d207847574b6c71e37006fd2c1cbd6cdee
SkuzzyxD <techdroidroot@gmail.com>
2020-07-29 19:11:30 +0530

    Update notes_db.py

--- pyrobot/utils/sql_helpers/notes_db.py
+++ pyrobot/utils/sql_helpers/notes_db.py
@@ -1,114 +1,67 @@
 import threading
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, func, distinct
 from pyrobot.utils.sql_helpers import SESSION, BASE
-from pyrobot.utils.msg_types import Types
 
-class SelfNotes(BASE):
-    __tablename__ = "self_notes"
-    user_id = Column(Integer, primary_key=True)
+
+class Notes(BASE):
+    __tablename__ = "notes"
     name = Column(UnicodeText, primary_key=True)
-    value = Column(UnicodeText, nullable=False)
-    msgtype = Column(Integer, default=Types.TEXT)
-    file = Column(UnicodeText)
+    d_message_id = Column(Integer)
 
-    def __init__(self, user_id, name, value, msgtype, file):
-        """initializing db"""
-        self.user_id = user_id
+    def __init__(self, name, d_message_id):
         self.name = name
-        self.value = value
-        self.msgtype = msgtype
-        self.file = file
+        self.d_message_id = d_message_id
 
     def __repr__(self):
-        """get db message"""
         return "<Note %s>" % self.name
 
 
-SelfNotes.__table__.create(checkfirst=True)
+Notes.__table__.create(checkfirst=True)
 
-INSERTION_LOCK = threading.RLock()
-
-SELF_NOTES = {}
+NOTES_INSERTION_LOCK = threading.RLock()
 
 
-# Types of message
-# TEXT = 1
-# DOCUMENT = 2
-# PHOTO = 3
-# VIDEO = 4
-# STICKER = 5
-# AUDIO = 6
-# VOICE = 7
-# VIDEO_NOTE = 8
-# ANIMATION = 9
-# ANIMATED_STICKER = 10
-# CONTACT = 11
-
-def save_selfnote(user_id, note_name, note_data, msgtype, file=None):
-    global SELF_NOTES
-    with INSERTION_LOCK:
-        prev = SESSION.query(SelfNotes).get((user_id, note_name))
-        if prev:
+def add_note_to_db(note_name, note_message_id):
+    with NOTES_INSERTION_LOCK:
+        prev_note = SESSION.query(Notes).get(note_name)
+        if prev_note:
             SESSION.delete(prev)
-        note = SelfNotes(user_id, note_name, note_data, msgtype=int(msgtype), file=file)
+        note = Notes(note_name, note_message_id)
         SESSION.add(note)
         SESSION.commit()
 
-        if not SELF_NOTES.get(user_id):
-            SELF_NOTES[user_id] = {}
-        SELF_NOTES[user_id][note_name] = {'value': note_data, 'type': msgtype, 'file': file}
+
+def get_note(note_name):
+    try:
+        note = SESSION.query(Notes).get(note_name)
+        return note
+    finally:
+        SESSION.close()
 
 
-def get_selfnote(user_id, note_name):
-    if not SELF_NOTES.get(user_id):
-        SELF_NOTES[user_id] = {}
-    return SELF_NOTES[user_id].get(note_name)
-
-
-def get_all_selfnotes(user_id):
-    if not SELF_NOTES.get(user_id):
-        SELF_NOTES[user_id] = {}
-        return None
-    allnotes = list(SELF_NOTES[user_id])
-    allnotes.sort()
-    return allnotes
-
-
-def get_all_selfnotes_inline(user_id):
-    if not SELF_NOTES.get(user_id):
-        SELF_NOTES[user_id] = {}
-        return None
-    # Sorting
-    allnotes = {}
-    sortnotes = list(SELF_NOTES[user_id])
-    sortnotes.sort()
-    for x in sortnotes:
-        allnotes[x] = SELF_NOTES[user_id][x]
-    return allnotes
-
-
-def rm_selfnote(user_id, note_name):
-    global SELF_NOTES
-    with INSERTION_LOCK:
-        note = SESSION.query(SelfNotes).get((user_id, note_name))
+def rm_note(note_name):
+    with NOTES_INSERTION_LOCK:
+        note = SESSION.query(Notes).get(note_name)
         if note:
             SESSION.delete(note)
             SESSION.commit()
-            SELF_NOTES[user_id].pop(note_name)
             return True
-
         else:
             SESSION.close()
             return False
 
 
-def __load_allnotes():
-    global SELF_NOTES
-    getall = SESSION.query(SelfNotes).distinct().all()
-    for x in getall:
-        if not SELF_NOTES.get(x.user_id):
-            SELF_NOTES[x.user_id] = {}
-        SELF_NOTES[x.user_id][x.name] = {'value': x.value, 'type': x.msgtype, 'file': x.file}
+def get_all_chat_notes():
+    try:
+        all_notes = SESSION.query(Notes).order_by(Notes.name.asc()).all()
+        return all_notes
+    finally:
+        SESSION.close()
 
 
-__load_allnotes()
+def num_notes():
+    try:
+        notes_count = SESSION.query(Notes).count()
+        return notes_count
+    finally:
+        SESSION.close()

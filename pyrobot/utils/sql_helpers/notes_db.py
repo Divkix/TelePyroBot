bd30898ff2742eaa74acd944662ae09ab0cf8336
SkuzzyxD <techdroidroot@gmail.com>
2020-07-29 21:21:43 +0530

    Update notes_db.py

--- pyrobot/utils/sql_helpers/notes_db.py
+++ pyrobot/utils/sql_helpers/notes_db.py
@@ -1,69 +1,117 @@
-# Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, func, distinct
+from sqlalchemy import Column, UnicodeText, Integer
 
-from pyrobot.utils.sql_helpers import SESSION, BASE
+from pyrobot import BASE, SESSION
+from pyrobot.utils.msg_types import Types
 
 
-class Notes(BASE):
-    __tablename__ = "notes"
-    chat_id = Column(String, primary_key=True)
+class SelfNotes(BASE):
+    __tablename__ = "self_notes"
+    user_id = Column(Integer, primary_key=True)
     name = Column(UnicodeText, primary_key=True)
-    data = Column(String)
+    value = Column(UnicodeText, nullable=False)
+    msgtype = Column(Integer, default=Types.TEXT)
+    file = Column(UnicodeText)
 
-    def __init__(self, chat_id, name, d_message_id):
-        self.chat_id = str(chat_id)
+    def __init__(self, user_id, name, value, msgtype, file):
+        """initializing db"""
+        self.user_id = user_id
         self.name = name
-        self.data = str(data)
+        self.value = value
+        self.msgtype = msgtype
+        self.file = file
 
     def __repr__(self):
+        """get db message"""
         return "<Note %s>" % self.name
 
 
-Notes.__table__.create(checkfirst=True)
+SelfNotes.__table__.create(checkfirst=True)
 
-NOTES_INSERTION_LOCK = threading.RLock()
+INSERTION_LOCK = threading.RLock()
+
+SELF_NOTES = {}
 
 
-def add_note_to_db(chat_id, note_name, note_message_id):
-    with NOTES_INSERTION_LOCK:
-        prev = SESSION.query(Notes).get((str(chat_id), note_name))
+# Types of message
+# TEXT = 1
+# DOCUMENT = 2
+# PHOTO = 3
+# VIDEO = 4
+# STICKER = 5
+# AUDIO = 6
+# VOICE = 7
+# VIDEO_NOTE = 8
+# ANIMATION = 9
+# ANIMATED_STICKER = 10
+# CONTACT = 11
+
+def save_selfnote(user_id, note_name, note_data, msgtype, file=None):
+    global SELF_NOTES
+    with INSERTION_LOCK:
+        prev = SESSION.query(SelfNotes).get((user_id, note_name))
         if prev:
             SESSION.delete(prev)
-        note = Notes(str(chat_id), note_name, note_message_id)
+        note = SelfNotes(user_id, note_name, note_data, msgtype=int(msgtype), file=file)
         SESSION.add(note)
         SESSION.commit()
 
-
-def get_note(chat_id, note_name):
-    try:
-        return SESSION.query(Notes).get((str(chat_id), note_name))
-    finally:
-        SESSION.close()
+        if not SELF_NOTES.get(user_id):
+            SELF_NOTES[user_id] = {}
+        SELF_NOTES[user_id][note_name] = {'value': note_data, 'type': msgtype, 'file': file}
 
 
-def rm_note(chat_id, note_name):
-    with NOTES_INSERTION_LOCK:
-        note = SESSION.query(Notes).get((str(chat_id), note_name))
+def get_selfnote(user_id, note_name):
+    if not SELF_NOTES.get(user_id):
+        SELF_NOTES[user_id] = {}
+    return SELF_NOTES[user_id].get(note_name)
+
+
+def get_all_selfnotes(user_id):
+    if not SELF_NOTES.get(user_id):
+        SELF_NOTES[user_id] = {}
+        return None
+    allnotes = list(SELF_NOTES[user_id])
+    allnotes.sort()
+    return allnotes
+
+
+def get_all_selfnotes_inline(user_id):
+    if not SELF_NOTES.get(user_id):
+        SELF_NOTES[user_id] = {}
+        return None
+    # Sorting
+    allnotes = {}
+    sortnotes = list(SELF_NOTES[user_id])
+    sortnotes.sort()
+    for x in sortnotes:
+        allnotes[x] = SELF_NOTES[user_id][x]
+    return allnotes
+
+
+def rm_selfnote(user_id, note_name):
+    global SELF_NOTES
+    with INSERTION_LOCK:
+        note = SESSION.query(SelfNotes).get((user_id, note_name))
         if note:
             SESSION.delete(note)
             SESSION.commit()
+            SELF_NOTES[user_id].pop(note_name)
             return True
+
         else:
             SESSION.close()
             return False
 
 
-def get_all_chat_notes(chat_id):
-    try:
-        return SESSION.query(Notes).filter(Notes.chat_id == str(chat_id)).order_by(Notes.name.asc()).all()
-    finally:
-        SESSION.close()
+def __load_allnotes():
+    global SELF_NOTES
+    getall = SESSION.query(SelfNotes).distinct().all()
+    for x in getall:
+        if not SELF_NOTES.get(x.user_id):
+            SELF_NOTES[x.user_id] = {}
+        SELF_NOTES[x.user_id][x.name] = {'value': x.value, 'type': x.msgtype, 'file': x.file}
 
 
-def num_notes():
-    try:
-        return SESSION.query(Notes).count()
-    finally:
-        SESSION.close()
+__load_allnotes()

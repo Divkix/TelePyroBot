14d5d7c95bc4d92e3452be4df41d72e9022f626f
Skuzzy xD <techdroidroot@gmail.com>
2020-07-30 10:50:19 +0530

    fix notes with defining file_ref, file_id and chat_id in notes.py

--- pyrobot/plugins/notes.py
+++ pyrobot/plugins/notes.py
@@ -79,7 +79,7 @@ async def get_note(client, message):
         if teks:
             await message.edit(teks)
     elif getnotes['type'] in (Types.STICKER, Types.VOICE, Types.VIDEO_NOTE, Types.CONTACT, Types.ANIMATED_STICKER):
-        await GET_FORMAT[getnotes['type']](message.chat.id, getnotes['file_id'], getnotes['file_ref'], reply_to_message_id=ReplyCheck(message))
+        await GET_FORMAT[getnotes['type']](chat_id=message.chat.id, file_id=getnotes['file_id'], file_ref=getnotes['file_ref'], reply_to_message_id=ReplyCheck(message))
     else:
         if getnotes.get('value'):
             teks = getnotes.get('value')

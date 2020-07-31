192adb5413d4eeabd3a4978d670939fc745d6294
SkuzzyxD <techdroidroot@gmail.com>
2020-07-31 23:52:46 +0530

    Update pyrohelpers.py

--- pyrobot/utils/pyrohelpers.py
+++ pyrobot/utils/pyrohelpers.py
@@ -12,15 +12,21 @@ def ReplyCheck(message: Message):
 
     return reply_id
 
-def extract_user(message: Message) -> (int, str):
+async def extract_user(client, message: Message) -> (int, str):
     user_id = None
     user_first_name = None
+
+    if len(message.command) == 2 and not message.reply_to_message and message.command[1].startswith("@"):
+        user = await client.get_users(message.command[1])
+        user_id = user.id
+        user_first_name = user.first_name
+    return user_id, user_first_name
 
     if message.reply_to_message:
         user_id = message.reply_to_message.from_user.id
         user_first_name = message.reply_to_message.from_user.first_name
 
-    elif len(message.command) > 1:
+    elif len(message.command) > 1 and not message.command[1].startswith("@"):
         if len(message.entities) >= 1:
             required_entity = message.entities[-1]
             if required_entity.type == "text_mention":

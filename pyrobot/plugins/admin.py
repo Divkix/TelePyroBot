47eb11cdbe931b113dfd72a363fe14d7b80363f3
SkuzzyxD <techdroidroot@gmail.com>
2020-08-01 00:04:12 +0530

    Update admin.py

--- pyrobot/plugins/admin.py
+++ pyrobot/plugins/admin.py
@@ -103,6 +103,7 @@ async def ban_usr(client, message):
         await message.delete()
         return
     user_id, user_first_name = await extract_user(client, message)
+    await client.delete_messages(message.chat.id, message.reply_to_message.message_id)
     try:
         await message.chat.kick_member(user_id=user_id)
         if str(user_id).lower().startswith("@"):

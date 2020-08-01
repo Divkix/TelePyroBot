9ddb709b1a81f7ec76c923875c9ee40f57d7af02
Skuzzy xD <techdroidroot@gmail.com>
2020-08-01 17:58:12 +0530

    Add last_msg_id - 1

--- pyrobot/utils/sql_helpers/pmpermit_db.py
+++ pyrobot/utils/sql_helpers/pmpermit_db.py
@@ -3,39 +3,42 @@ from pyrobot.utils.sql_helpers import BASE, SESSION
 import threading
 
 
-class WhitelistUsers(BASE):
+class PMTable(BASE):
     __tablename__ = "pmapprove"
     user_id = Column(Integer, primary_key=True)
     boolvalue = Column(String)
     msg_id = Column(Integer)
 
-    def __init__(self, user_id, boolvalue, msg_id=0):
+    def __init__(self, user_id, boolvalue, msg_id):
         """initializing db"""
         self.user_id = user_id
         self.boolvalue = boolvalue
         self.msg_id = msg_id
 
 
-WhitelistUsers.__table__.create(checkfirst=True)
+PMTable.__table__.create(checkfirst=True)
 INSERTION_LOCK = threading.RLock()
 
 def set_whitelist(user_id, boolvalue):
     with INSERTION_LOCK:
-        user = SESSION.query(WhitelistUsers).get(user_id)
-        if not user:
-            user = WhitelistUsers(user_id, boolvalue, msg_id)
-        else:
-            user.boolvalue = str(boolvalue)
-        SESSION.add(user)
-        SESSION.commit()
+        user = SESSION.query(PMTable).get(user_id)
+        try:
+            if not user:
+                user = PMTable(user_id, boolvalue, msg_id)
+            else:
+                user.boolvalue = str(boolvalue)
+            SESSION.add(user)
+            SESSION.commit()
+        finally:
+            SESSION.close()
     return user_id, msg_id
 
 
 def set_last_msg_id(user_id, msg_id):
     with INSERTION_LOCK:
-        user = SESSION.query(WhitelistUsers).get(user_id)
+        user = SESSION.query(PMTable).get(user_id)
         if not user:
-            user = WhitelistUsers(user_id, str(False), msg_id)
+            user = PMTable(user_id, str(False), msg_id)
         else:
             user.msg_id = msg_id
         SESSION.merge(user)
@@ -43,8 +46,19 @@ def set_last_msg_id(user_id, msg_id):
     return
 
 
+def del_whitelist(user_id):
+    with INSERTION_LOCK:
+        user = SESSION.query(PMTable).get(user_id)
+        try:
+            if user:
+                SESSION.delete(user)
+                SESSION.commit()
+        finally:
+            SESSION.close()
+        return False
+
 def get_msg_id(user_id):
-    user = SESSION.query(WhitelistUsers).get(str(user_id))
+    user = SESSION.query(PMTable).get(str(user_id))
     msg_id = None
     if user:
         msg_id = user.msg_id
@@ -52,19 +66,8 @@ def get_msg_id(user_id):
     return msg_id
 
 
-def del_whitelist(user_id):
-    with INSERTION_LOCK:
-        user = SESSION.query(WhitelistUsers).get(user_id)
-        if user:
-            SESSION.delete(user)
-            SESSION.commit()
-        else:
-            SESSION.close()
-            return False
-
-
 def get_whitelist(user_id):
-    user = SESSION.query(WhitelistUsers).get(user_id)
+    user = SESSION.query(PMTable).get(user_id)
     rep = ""
     if user:
         rep = str(user.boolvalue)

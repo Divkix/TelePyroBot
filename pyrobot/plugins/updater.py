b541320cc6f2838020079052f22e1e73eabe5122
Skuzzy xD <techdroidroot@gmail.com>
2020-08-04 22:43:08 +0530

    update updater with force push - 7 + changelogs

--- pyrobot/plugins/updater.py
+++ pyrobot/plugins/updater.py
@@ -10,11 +10,7 @@ from pyrobot import (
     HEROKU_APP_NAME,
     LOGGER,
     MAX_MESSAGE_LENGTH,
-    OFFICIAL_UPSTREAM_REPO,
-    OFFICIAL_BRANCH)
-
-from git import Repo
-from git.exc import InvalidGitRepositoryError, GitCommandError, NoSuchPathError
+    OFFICIAL_UPSTREAM_REPO)
 
 # -- Constants -- #
 IS_SELECTED_DIFFERENT_BRANCH = (
@@ -24,30 +20,23 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "please check out to an official branch, and re-start the updater.")
 REPO_REMOTE_NAME = "tmp_upstream_remote"
 IFFUCI_ACTIVE_BRANCH_NAME = "master"
+DIFF_MARKER = "HEAD..{remote_name}/{branch_name}"
 NO_HEROKU_APP_CFGD = "no heroku application found, but a key given? 😕 "
 HEROKU_GIT_REF_SPEC = "HEAD:refs/heads/master"
-UPDATE_IN_PROGRESS = f"**Updating Application!** __Please wait upto 5 minutes....__\n\nDo `{COMMAND_HAND_LER}alive` or `{COMMAND_HAND_LER}start` to check if I'm alive"
+BOT_IS_UP_TO_DATE = "**__TelePyroBot is already upto date!__**"
+NEW_BOT_UP_DATE_FOUND = "**NEW update found for** __{branch_name}__\n**Chagelog:**\n\n`{changelog}`\n__**Updating...**__"
+NEW_UP_DATE_FOUND = "**NEW Update found for** __{branch_name}__\n__**Updating ...**__"
 # -- Constants End -- #
 
 __PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
 
 __help__ = f"""
-Update your username to latest version!
-
 `{COMMAND_HAND_LER}update`: Update userbot to latest version.
 """
 
-async def gen_chlog(repo, diff):
-    changelog = ""
-    d_form = "%H:%M - %d/%m/%y"
-    for cl in repo.iter_commits(diff):
-        changelog += f'• [{cl.committed_datetime.strftime(d_form)}]: {cl.summary} <{cl.author}>\n'
-    return changelog
-
-
 @Client.on_message(Filters.command("update", COMMAND_HAND_LER) & Filters.me)
 async def updater(client, message):
-    await message.edit("`Checking for updating...`")
+    await message.edit("`Updating Please Wait...`")
     try:
         repo = git.Repo()
     except git.exc.InvalidGitRepositoryError as error_one:
@@ -59,10 +48,6 @@ async def updater(client, message):
         repo.heads.master.checkout(True)
 
     active_branch_name = repo.active_branch.name
-    if active_branch_name not in OFFICIAL_BRANCH:
-        await message.edit(f'**[UPDATER]:** Looks like you are using your own custom branch ({active_branch_name}). in that case, Updater is unable to identify which branch is to be merged. please checkout to any official branch')
-        return
-
     LOGGER.info(active_branch_name)
     if active_branch_name != IFFUCI_ACTIVE_BRANCH_NAME:
         await message.edit(IS_SELECTED_DIFFERENT_BRANCH.format(
@@ -77,22 +62,43 @@ async def updater(client, message):
 
     tmp_upstream_remote = repo.remote(REPO_REMOTE_NAME)
     tmp_upstream_remote.fetch(active_branch_name)
-    try:
-        changelog = await gen_chlog(repo, HEROKU_GIT_REF_SPEC)
-        changelog_file = "pyrobot/cache/changelog.txt"
-        with open(changelog_file, "w", encoding="utf-8") as f:
-            f.write(str(changelog))
-            f.close()
-        await message.reply_document(document=changelog_file,
-                               caption="Here is the chat list that you joined.")
-        os.remove(changelog_file)
-    except Exception as err:
-        if "fatal: bad revision" in str(err):
-            await message.edit("`Cannot send Changelog`")
-            pass
 
-    await asyncio.sleep(5)
-    await message.edit(UPDATE_IN_PROGRESS)
+    changelog = generate_change_log(
+        repo,
+        DIFF_MARKER.format(
+            remote_name=REPO_REMOTE_NAME,
+            branch_name=active_branch_name
+        )
+    )
+    LOGGER.info(changelog)
+
+    if not changelog:
+        await message.edit(BOT_IS_UP_TO_DATE)
+        return
+
+    message_one = NEW_BOT_UP_DATE_FOUND.format(
+        branch_name=active_branch_name,
+        changelog=changelog
+    )
+    message_two = NEW_UP_DATE_FOUND.format(
+        branch_name=active_branch_name
+    )
+
+    if len(message_one) > MAX_MESSAGE_LENGTH:
+        with open("change.log", "w+", encoding="utf8") as out_file:
+            out_file.write(str(message_one))
+        await message.reply_document(
+            document="change.log",
+            caption=message_two,
+            disable_notification=True,
+            reply_to_message_id=message.message_id
+        )
+        os.remove("change.log")
+    else:
+        await message.reply(message_one)
+
+    await asyncio.sleep(3)
+    tmp_upstream_remote.fetch(active_branch_name)
     repo.git.reset("--hard", "FETCH_HEAD")
 
     if HEROKU_API_KEY is not None:
@@ -111,3 +117,22 @@ async def updater(client, message):
         remote.push(refspec=HEROKU_GIT_REF_SPEC, force=True)
     else:
         await message.edit(NO_HEROKU_APP_CFGD)
+
+    asyncio.get_event_loop().create_task(restart(client, status_message))
+
+
+def generate_change_log(git_repo, diff_marker):
+    out_put_str = ""
+    d_form = "%d/%m/%y"
+    for repo_change in git_repo.iter_commits(diff_marker):
+        out_put_str += f"•[{repo_change.committed_datetime.strftime(d_form)}]: "
+        out_put_str += f"{repo_change.summary} <{repo_change.author}>\n"
+    return out_put_str
+
+
+async def restart(client, message):
+    await client.restart()
+    await message.edit(
+        "**Restarted!**\n"
+        f"do `{COMMAND_HAND_LER}alive` to check if I am online :p"
+    )

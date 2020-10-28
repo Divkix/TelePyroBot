fd85d948d881a0b0d815b288b9eb20a089c4bea7
Skuzzy xD <23524935+SkuzzyxD@users.noreply.github.com>
2020-10-28 17:57:01 +0530

    alt login method

--- telepyrobot/plugins/meganz.py
+++ telepyrobot/plugins/meganz.py
@@ -21,16 +21,22 @@ __HELP__ = f"""
 `{COMMAND_HAND_LER}megafind <filename>`: Find file or folder in mega drive.
 `{COMMAND_HAND_LER}megaup <file location>`: Upload the file and export its link.
 """
+mega, megaC = None, None
 
-
-mega = Mega()
-try:
-    if MEGANZ_EMAIL and MEGANZ_PASSWORD:
-        megaC = mega.login(MEGANZ_EMAIL, MEGANZ_PASSWORD)
-    else:
-        megaC = mega.login()
-except Exception as ef:
-    LOGGER.info(ef)
+def megaLogin():
+    global mega, megaC
+    if not mega or megaC:
+        del mega
+        del megaC
+        mega = Mega()
+        try:
+            if MEGANZ_EMAIL and MEGANZ_PASSWORD:
+                megaC = mega.login(MEGANZ_EMAIL, MEGANZ_PASSWORD)
+            else:
+                megaC = mega.login()
+        except Exception as ef:
+            LOGGER.info(ef)
+   return
 
 
 @TelePyroBot.on_message(filters.command("megainfo", COMMAND_HAND_LER) & filters.me)
@@ -40,6 +46,7 @@ async def mega_info(c: TelePyroBot, m: Message):
             "Setup `MEGANZ_EMAIL` and `MEGANZ_PASSWORD` vars to use this."
         )
         return
+    megaLogin()
     details = f"User:\n{megaC.get_user()}\n\n"
     details += f"Quota:\n{megaC.get_quota()}\n\n"
     details += f"Storage:\n{megaC.get_storage_space(kilo=True)}"
@@ -54,6 +61,7 @@ async def mega_ls(c: TelePyroBot, m: Message):
             "Setup `MEGANZ_EMAIL` and `MEGANZ_PASSWORD` vars to use this."
         )
         return
+    megaLogin()
     files = f"Files\n{megaC.get_files()}"
     await m.edit_text(files)
     return
@@ -61,6 +69,7 @@ async def mega_ls(c: TelePyroBot, m: Message):
 
 @TelePyroBot.on_message(filters.command("megadl", COMMAND_HAND_LER) & filters.me)
 async def mega_dl(c: TelePyroBot, m: Message):
+    megaLogin()
     if len(m.command) == 2:
         dlurl = m.text.split(" ", 1)[1]
         if ("https://mega.co.nz" or "mega.co.nz", "mega.nz") in dl_url:
@@ -70,6 +79,12 @@ async def mega_dl(c: TelePyroBot, m: Message):
 
 @TelePyroBot.on_message(filters.command("megafind", COMMAND_HAND_LER) & filters.me)
 async def mega_find(c: TelePyroBot, m: Message):
+    if not (MEGANZ_EMAIL and MEGANZ_PASSWORD):
+        await m.edit_text(
+            "Setup `MEGANZ_EMAIL` and `MEGANZ_PASSWORD` vars to use this."
+        )
+        return
+    megaLogin()
     if len(m.command) == 2:
         foldername = m.text.split(" ", 1)[1]
         folder = megaC.find(foldername)
@@ -79,6 +94,12 @@ async def mega_find(c: TelePyroBot, m: Message):
 
 @TelePyroBot.on_message(filters.command("megaup", COMMAND_HAND_LER) & filters.me)
 async def mega_find(c: TelePyroBot, m: Message):
+    if not (MEGANZ_EMAIL and MEGANZ_PASSWORD):
+        await m.edit_text(
+            "Setup `MEGANZ_EMAIL` and `MEGANZ_PASSWORD` vars to use this."
+        )
+        return
+    megaLogin()
     if len(m.text.split) >= 2:
         await m.reply_text("Uploading file...")
         fileLoc = m.text.split(" ", 2)

e57a179c430639740dc0ab2726319228969b81ac
Skuzzy xD <23524935+SkuzzyxD@users.noreply.github.com>
2020-10-28 08:27:46 +0530

    Update download_upload.py

--- telepyrobot/plugins/download_upload.py
+++ telepyrobot/plugins/download_upload.py
@@ -42,7 +42,7 @@ The command will upload all files from the directory location to the current Tel
 @TelePyroBot.on_message(
     filters.command(["download", "dl"], COMMAND_HAND_LER) & filters.me
 )
-async def down_load_media(client, sms):
+async def down_load_media(c: Client, sms: Message):
     message = await sms.reply_text("...", quote=True)
     if not os.path.isdir(TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TMP_DOWNLOAD_DIRECTORY)

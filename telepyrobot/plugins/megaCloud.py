6891c9da97bb984565f18a948aa5459c52917bf7
Skuzzy xD <23524935+SkuzzyxD@users.noreply.github.com>
2020-10-28 17:46:21 +0530

    Add upload, help and plugin name

--- telepyrobot/plugins/megaCloud.py
+++ telepyrobot/plugins/megaCloud.py
@@ -1,3 +1,4 @@
+import os
 from telepyrobot.__main__ import TelePyroBot
 from pyrogram import filters
 from pyrogram.types import Message
@@ -10,8 +11,19 @@ from telepyrobot import (
 )
 from mega import Mega
 
-mega = Mega()
 
+__PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
+
+__HELP__ = f"""
+`{COMMAND_HAND_LER}megainfo`: Get account info!
+`{COMMAND_HAND_LER}megals`: List files on mega.nz.
+`{COMMAND_HAND_LER}megadl` / `{COMMAND_HAND_LER}dlmega`: Download file from mega.nz link.
+`{COMMAND_HAND_LER}megafind <filename>`: Find file or folder in mega drive.
+`{COMMAND_HAND_LER}megaup <file location>`: Upload the file and export its link.
+"""
+
+
+mega = Mega()
 try:
     if MEGANZ_EMAIL and MEGANZ_PASSWORD:
         megaC = mega.login(MEGANZ_EMAIL, MEGANZ_PASSWORD)
@@ -24,36 +36,34 @@ except Exception as ef:
 @TelePyroBot.on_message(filters.command("megainfo", COMMAND_HAND_LER) & filters.me)
 async def mega_info(c: TelePyroBot, m: Message):
     if not (MEGANZ_EMAIL and MEGANZ_PASSWORD):
-        await m.reply_text(
+        await m.edit_text(
             "Setup `MEGANZ_EMAIL` and `MEGANZ_PASSWORD` vars to use this."
         )
         return
     details = f"User:\n{megaC.get_user()}\n\n"
     details += f"Quota:\n{megaC.get_quota()}\n\n"
     details += f"Storage:\n{megaC.get_storage_space(kilo=True)}"
-    await m.reply_text(details)
+    await m.edit_text(details)
     return
 
 
 @TelePyroBot.on_message(filters.command("megals", COMMAND_HAND_LER) & filters.me)
 async def mega_ls(c: TelePyroBot, m: Message):
     if not (MEGANZ_EMAIL and MEGANZ_PASSWORD):
-        await m.reply_text(
+        await m.edit_text(
             "Setup `MEGANZ_EMAIL` and `MEGANZ_PASSWORD` vars to use this."
         )
         return
     files = f"Files\n{megaC.get_files()}"
-    await m.reply_text(files)
+    await m.edit_text(files)
     return
 
 
-@TelePyroBot.on_message(
-    filters.command(["megadl", "dlmega"], COMMAND_HAND_LER) & filters.me
-)
+@TelePyroBot.on_message(filters.command("megadl", COMMAND_HAND_LER) & filters.me)
 async def mega_dl(c: TelePyroBot, m: Message):
     if len(m.command) == 2:
         dlurl = m.text.split(" ", 1)[1]
-        if dlurl.startswith("https://mega.co.nz"):
+        if ("https://mega.co.nz" or "mega.co.nz", "mega.nz") in dl_url:
             megaC.download_url(dlurl, TMP_DOWNLOAD_DIRECTORY)
     return
 
@@ -63,6 +73,19 @@ async def mega_find(c: TelePyroBot, m: Message):
     if len(m.command) == 2:
         foldername = m.text.split(" ", 1)[1]
         folder = megaC.find(foldername)
-        await m.reply_text(f"Searched for: {foldername}\n\nResults:\n{folder}")
+        await m.edit_text(f"Searched for: {foldername}\n\nResults:\n{folder}")
+    return
 
+
+@TelePyroBot.on_message(filters.command("megaup", COMMAND_HAND_LER) & filters.me)
+async def mega_find(c: TelePyroBot, m: Message):
+    if len(m.text.split) >= 2:
+        await m.reply_text("Uploading file...")
+        fileLoc = m.text.split(" ", 2)
+        try:
+            file = megaC.upload(fileLoc)
+            link = megaC.get_upload_link(file)
+        except Exception as ef:
+            await m.edit_text(ef)
+        await m.edit_text(f"File <i>{fileLoc}</i> Uploaded!\n\n<b>Link:</b> {link}")
     return

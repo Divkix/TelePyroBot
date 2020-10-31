3a32eaf05c72fb26bf5ee75a6940495cb5c85a74
Skuzzy xD <techdroidroot@gmail.com>
2020-10-31 18:54:02 +0530

    size

---
+++ telepyrobot/utils/check_size.py
@@ -0,0 +1,12 @@
+def get_size_format(b, factor=1024, suffix="B"):
+    """
+    Scale bytes to its proper byte format
+    e.g:
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+        if b < factor:
+            return f"{b:.2f}{unit}{suffix}"
+        b /= factor
+    return f"{b:.2f}Y{suffix}"

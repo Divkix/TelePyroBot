587687331403ec98b1dec469d280138b6cc0c75e
Skuzzy xD <techdroidroot@gmail.com>
2020-10-31 18:56:52 +0530

    get size test

--- telepyrobot/plugins/list_directory.py
+++ telepyrobot/plugins/list_directory.py
@@ -4,6 +4,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 from telepyrobot import MAX_MESSAGE_LENGTH, COMMAND_HAND_LER
 from telepyrobot.utils.clear_string import clear_string
+from telepyrobot.utils.check_size import get_size_format
 
 
 __PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
@@ -15,13 +16,26 @@ List the directories of the server.
 `{COMMAND_HAND_LER}ls <diectory name>`: List all the files in the directory.
 """
 
-def size(loc):
-    t = 0
-    for  dirpath, dirnames, filenames in os.walk(loc):
-        for i in filenames:
-            f = os.path.join(dirpath, i)
-            t += os.path.getsize(f)
-    return t
+def get_directory_size(directory):
+    """Returns the `directory` size in bytes."""
+    total = 0
+    try:
+        # print("[+] Getting the size of", directory)
+        for entry in os.scandir(directory):
+            if entry.is_file():
+                # if it's a file, use stat() function
+                total += entry.stat().st_size
+            elif entry.is_dir():
+                # if it's a directory, recursively call this function
+                total += get_directory_size(entry.path)
+    except NotADirectoryError:
+        # if `directory` isn't a directory, get the file size then
+        return os.path.getsize(directory)
+    except PermissionError:
+        # if for whatever reason we can't open the folder, return 0
+        return 0
+    return get_size_format(total)
+
 
 @TelePyroBot.on_message(filters.command("ls", COMMAND_HAND_LER) & filters.me)
 async def list_directories(c: TelePyroBot, m: Message):
@@ -37,7 +51,7 @@ async def list_directories(c: TelePyroBot, m: Message):
     files.sort()  # Sort the files
 
     for file in files:
-        OUTPUT += f"<code>{file}</code>\n"
+        OUTPUT += f"<code>{file}</code> ({get_directory_size(location)})\n"
 
     if len(OUTPUT) > MAX_MESSAGE_LENGTH:
         # OUTPUT = clear_string(OUTPUT)  # Remove the html elements using regex
@@ -45,7 +59,7 @@ async def list_directories(c: TelePyroBot, m: Message):
             out_file.write(OUTPUT)
         await m.reply_document(
             document="ls.txt",
-            caption=f"{location} ({size(location)})")
+            caption=f"{location} ({get_directory_size(location)})")
         await m.delete()
         os.remove("ls.txt")
     else:

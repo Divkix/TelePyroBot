5ab2be73b188ee934fbf23018e805fb567ce61a6
Skuzzy xD <techdroidroot@gmail.com>
2020-11-01 18:54:36 +0530

    ytv_dl change

--- telepyrobot/plugins/utube.py
+++ telepyrobot/plugins/utube.py
@@ -60,7 +60,7 @@ Youtube Downloader using youtube-dl python library!
 `{COMMAND_HAND_LER}ytv <link>`: Download Video from YouTube.
 """
 
-yt_opts = {
+ytv_opts = {
     "verbose": True,
     "merge_output_format": "mkv",
     "geo_bypass": True,
@@ -73,7 +73,7 @@ yt_opts = {
 
 
 @TelePyroBot.on_message(filters.command("ytv", COMMAND_HAND_LER) & filters.me)
-async def tor_search(c: TelePyroBot, m: Message):
+async def ytv_dl(c: TelePyroBot, m: Message):
     link = m.text.split(None, 1)[1]
     if ("youtube.com" or "youtu.be") in link:
         await m.edit_text("<code>Getting Video Information...</code>")
@@ -116,7 +116,7 @@ async def tor_search(c: TelePyroBot, m: Message):
         #     await m.edit_text(OUTPUT)
         # return
         try:
-            with YoutubeDL(yt_opts) as ydl:
+            with YoutubeDL(ytv_opts) as ydl:
                 ydl.download(link)
         except Exception:
             exc = traceback.format_exc()

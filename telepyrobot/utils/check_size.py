00290b05cfac8ad8f3820a5a0b77897bf80c61f9
Skuzzy xD <techdroidroot@gmail.com>
2020-11-01 10:41:42 +0530

    Update check_size.py

--- telepyrobot/utils/check_size.py
+++ telepyrobot/utils/check_size.py
@@ -25,12 +25,6 @@ def get_directory_size(location):
     return get_size_format(size)
 
 def get_size_format(b, factor=1024, suffix="B"):
-    """
-    Scale bytes to its proper byte format
-    e.g:
-        1253656 => '1.20MB'
-        1253656678 => '1.17GB'
-    """
     for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
         if b < factor:
             return f"{b:.2f}{unit}{suffix}"

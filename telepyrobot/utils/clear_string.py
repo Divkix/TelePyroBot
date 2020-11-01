49ccaf8ff993f9650ac5b59e9b6415f878d29c0b
Skuzzy xD <techdroidroot@gmail.com>
2020-11-01 08:53:19 +0530

    Update clear_string.py

--- telepyrobot/utils/clear_string.py
+++ telepyrobot/utils/clear_string.py
@@ -7,5 +7,5 @@ def clear_string(msg: str):
     msg = re.sub(r"\<b\>(.*)\<\/b\>", "\g<1>", msg)
     msg = re.sub(r"\*\*(.*)\*\*", "\g<1>", msg)
     msg = re.sub(r"\_\_(.*)\_\_", "\g<1>", msg)
-    msg = re.sub(r"\`(.*\`", "\g<1>", msg)
+    msg = re.sub(r"\`(.*)\`", "\g<1>", msg)
     return msg

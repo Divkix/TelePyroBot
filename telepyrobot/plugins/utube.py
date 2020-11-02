196df58169d056a7284b93c5909dc419020e0055
Skuzzy xD <techdroidroot@gmail.com>
2020-11-02 13:28:52 +0530

    ytp options fix

--- telepyrobot/plugins/utube.py
+++ telepyrobot/plugins/utube.py
@@ -39,17 +39,6 @@ ytv_opts = {
     "format": "(bestvideo[height>=720]+bestaudio/bestvideo+bestaudio)",
 }
 
-ytp_opts = {
-    "verbose": True,
-    "merge_output_format": "mkv",
-    "geo_bypass": True,
-    "outtmpl": "/root/telepyrobot/cache/ytp/%(id)s/%(title)s.%(ext)s",
-    "restrictfilenames": True,
-    "writeautomaticsub": True,
-    "writedescription": True,
-    "format": "(bestvideo[height>=720]+bestaudio/bestvideo+bestaudio)",
-}
-
 yta_opts = {
     "verbose": True,
     "writethumbnail": True,
@@ -230,6 +219,18 @@ async def ytp_dl(c: TelePyroBot, m: Message):
         Errorss = ""
 
         for p in entries:
+            ytp_opts = {
+                "verbose": True,
+                "merge_output_format": "mkv",
+                "geo_bypass": True,
+                "outtmpl": "/root/telepyrobot/cache/ytp/{}/{}.{}".format(
+                    vid, p["title"], "mkv"
+                ),
+                "restrictfilenames": True,
+                "writeautomaticsub": True,
+                "writedescription": True,
+                "format": "(bestvideo[height>=720]+bestaudio/bestvideo+bestaudio)",
+            }
             try:
                 lk = p["webpage_url"]
                 with YoutubeDL(ytp_opts) as ydl:
